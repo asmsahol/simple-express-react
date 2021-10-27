ea057e38f667faef74c6adbcac23029b1df31c23
asmsahol <sahol.bin.mijan@gmail.com>
2021-10-27 14:40:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  const hundleAddUser = e => {
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const newUser = { name: name, email: email };
+
+    //send to the server
+    fetch("http://localhost:4000/users", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then(res => res.json())
+      .then(data => {
+        const addedUser = data;
+        const newUser = [...users, addedUser];
+        setUsers(newUser);
+      });
+    e.preventDefault();
+  };
+  nameRef.current.value = "";
+  emailRef.current.value = "";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Total Users: {users.length}</h2>
+      <form onSubmit={hundleAddUser}>
+        <input type='text' ref={nameRef} placeholder='Your Name' />
+        <input type='email' ref={emailRef} placeholder='Your Email' />
+        <input type='submit' placeholder='submit' />
+      </form>
+
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
